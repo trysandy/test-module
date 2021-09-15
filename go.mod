2439419ab31683ae67e405db914e0c9d84040e3b
Andry Trisandy Mahany <andry.trisandy@gamatechno.com>
2021-09-15 18:34:52 +0700

    (#) major change

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/trysandy/test-module
+module github.com/trysandy/test-module/v2
 
 go 1.16
--- hello.go
+++ hello.go
@@ -1,5 +1,5 @@
 package test_module
 
-func Hello() string {
-	return "Helloooo Wooorldddd"
+func Hello(name string) string {
+	return "Helloooo " + name
 }
